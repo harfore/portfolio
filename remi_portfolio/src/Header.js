3ea35f471735ccd7cf90020247201ae5b354b1f6
harfore <remiharfoush@gmail.com>
2024-04-28 13:42:53 +0200

    add: responsiveness

--- remi_portfolio/src/Header.js
+++ remi_portfolio/src/Header.js
@@ -6,11 +6,11 @@ export function Header() {
         <header className='header'>
             <nav>
                 <ul>
-                    <li><a href='/'>HOME</a></li>
-                    <li><a href='/projects'>PROJECTS</a></li>
-                    <li><a href='/about'>ABOUT</a></li>
-                    <li><a href='/resume'>RESUME</a></li>
-                    <li><a href='/contact'>CONTACT</a></li>
+                    <li><a to='/'>HOME</a></li>
+                    <li><a to='/projects'>PROJECTS</a></li>
+                    <li><a to='/about'>ABOUT</a></li>
+                    <li><a to='/resume'>RESUME</a></li>
+                    <li><a to='/contact'>CONTACT</a></li>
                 </ul>
             </nav>
         </header>
--- remi_portfolio/src/Resume.js
+++ remi_portfolio/src/Resume.js
@@ -5,24 +5,29 @@ export function Resume() {
     const Education = [
         {
             title: "Computer Science - Bachelor's Degree",
-            years: "2023 - 2025",
+            place_and_years: "2023 - 2025, Ada Tech School - 75010 Paris",
             place: "Ada Tech School - 75010 Paris",
-            diploma: "RNCP Level VI- Application Designer and Developer",
+            diploma: "RNCP Level VI Diploma- Application Designer and Developer",
             info1: "",
             info2: ""
 
         },
         {
             title: "International Trade and administration",
-            years: "2020 - 2023",
-            place: "Paris-XII University",
+            place_and_years: "2020 - 2023, Paris-XII University",
             diploma: "Bachelor's Degree",
             info1: "Study of economics, international trade, marketing and law",
             info2: "Last semester: Erasmus Exchange in Prague's Czech University of Life Sciences"
         },
         {
+            title: "Sainte Elisabeth High School",
+            place_and_years: "2017 - 2020, Paris",
+            diploma: "Economic and Social High School Diploma, awarded with high honors",
+            info1: "Class Delegate for student life (2018 - 2019)"
+        },
+        {
             title: "British Council",
-            years: "2009 - 2019",
+            place_and_years: "2009 - 2019, Paris",
             place: "Paris",
             diploma: "Cambridge Advanced English (CAE) Diploma",
             info1: "Score: 200/210 (C2 Level)",
@@ -37,8 +42,7 @@ export function Resume() {
                 {Education.map((School, index) => (
                     <div className='school' key={index}>
                         <h2 className='description'>{School.title}</h2>
-                        <h2>{School.years}</h2>
-                        <p>{School.place}</p>
+                        <h2>{School.place_and_years}</h2>
                         <h3>{School.diploma}</h3>
                         <p className='description'>{School.info1}</p>
                         <p className='description'>{School.info2}</p>
@@ -46,7 +50,7 @@ export function Resume() {
                 ))}
             </div>
             <h1>Professional Experience</h1>
-            <div class="experience">
+            <div className="experience">
                 <h2 className='description'>Assistance Coordinator - Night Shift</h2>
                 <h2>June - September 2022 & 2023</h2>
                 <h2>AXA Assistance</h2>
